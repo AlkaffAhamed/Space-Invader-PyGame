85ccd8e86228f80e3089517c98e6fa3cffa51f98
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-14 18:08:04 +0800

    02 Gameloop with quit event - Use an infinite while loop with a condition to check for quit event - If there is a quit event, break from the loop

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ pygame.init()
 
 # Create the screen (800x600)
 screen = pygame.display.set_mode((800, 600))
+
+# Game Loop with Quit event
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
