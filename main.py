155861c0c932f3d24051d759c23c0a05fda6704f
AlkaffAhamed <alkaffahamed@hotmail.com>
2023-06-14 18:14:33 +0800

    03 Add title and set icon - `pygame.display.set_caption()` for setting the title - Load the image by `icon = pygame.image.load("res/ufo.png")` - Set the image as icon: `pygame.display.set_icon(icon)`

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 # Create the screen (800x600)
 screen = pygame.display.set_mode((800, 600))
 
+# Title and icon
+pygame.display.set_caption("Space Invader PyGame")
+icon = pygame.image.load("res/ufo.png")
+pygame.display.set_icon(icon)
+
 # Game Loop with Quit event
 running = True
 while running:
